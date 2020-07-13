87bf88ba89d794ba7bd0ec097f88bdf9868c2c9d
dSecurityRep <68229432+dSecurityRep@users.noreply.github.com>
2020-07-13 18:29:04 +0300

    Update pass.py

--- pass.py
+++ pass.py
@@ -4,19 +4,27 @@ import os.path
 
 nick = str(input("Enter mickname: "))
 date = str(input("Enter year of birth: "))
+name = str(input("Enter name: "))
 
 #generating passwords
+
+x = "x123"
+z = "z123"
 
 v1 = nick + date
 v2 = date + nick
 
-v3 = nick + "x123"
+v3 = nick + x
 
-v4 = nick + "z123"
+v4 = nick + z
+
+v5 = name + x
+
+v6 = name + z
 
 #creating brutt base
 
-passwords = [v1, v2, v3, v4]
+passwords = [v1, v2, v3, v4, v5, v6]
 
 for p in passwords:
 	print(p)
